1aa21e05f9428b941f0383057fe4f3d2e6aadef9
ZhuXilin <delxmax@gmail.com>
2026-08-13 04:00:50 +0800

    Sorted script content

--- function/resource/MusicConfig.gd
+++ function/resource/MusicConfig.gd
@@ -2,11 +2,14 @@
 extends Resource
 class_name MusicConfig
 
+# ---- 场景音乐 ----
 @export var main_menu_music : AudioStream
 @export var camp_music : AudioStream
 @export var unit_select_music : AudioStream
 @export var map_music : AudioStream
 @export var non_combat_music : AudioStream
+
+# ---- 战斗音乐 ----
 @export var player_turn_music : AudioStream
 @export var enemy_turn_music : AudioStream
 @export var boss_player_turn_music : AudioStream
@@ -14,5 +17,7 @@ class_name MusicConfig
 @export var victory_music : AudioStream
 @export var defeat_music : AudioStream
 @export var win_game_music : AudioStream
+
+# ---- 对话音乐 ----
 @export var dialogue_music : AudioStream
 @export var battle_start_dialogue_music : AudioStream
